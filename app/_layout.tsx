3f0e3ae697e74940ead7bfa5a6d80ed16d6390d5
Hartz-byte <67004713+Hartz-byte@users.noreply.github.com>
2024-09-15 13:15:39 +0530

    project setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,30 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  // font load
+  const [loaded] = useFonts({
+    "Bangers-Regular": require("../assets/fonts/Bangers-Regular.ttf"),
+    "Okra-Bold": require("../assets/fonts/Okra-Bold.ttf"),
+    "Okra-Medium": require("../assets/fonts/Okra-Medium.ttf"),
+    "Okra-MediumLight": require("../assets/fonts/Okra-MediumLight.ttf"),
+    "Okra-Regular": require("../assets/fonts/Okra-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
